854d1386dc36e8139eb31c57443eac990b999403
Taehwa Kim <hi.taehwa@gmail.com>
2023-07-28 00:25:12 +0200

    feat: set sepolia deployment via alchemy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import '@openzeppelin/hardhat-upgrades';
+import 'dotenv/config';
+
+const { API_URL, PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
 
 export default config;
