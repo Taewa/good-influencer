4ccb633f2ae6c28f386df09491f3d98c14bb4788
Taehwa Kim <hi.taehwa@gmail.com>
2023-11-19 01:23:46 +0100

    add explanation on FE main page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -11,12 +11,23 @@ export default function MainPage() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24 pt-10">
+      <div className="max-w-xl mb-10">
+        <h1 className="text-2xl font-bold mb-2">Good Influencer</h1>
+        <p>
+          Have you ever felt like "thank you so much!" is not enough for someone who constantly helps you at work? 
+          Sometimes you have a co-worker who is passionate and encourages other co-workers which leads your project much in positive ways. 
+          I call them good influencers and believe they deserve more. Therefore I created this app which influencers can receive 
+          donations as Ether. Also the influencers will receive a trophy (it's actually ERC20 token) whenever they get a donation which will permanently carved in 
+          the Blockchain and this can be a proof of great team player!
+        </p>
+      </div>
+      
       <button 
-        className='p-6 mb-4 rounded-md bg-teal-700'
+        className='p-6 mb-4 rounded-md bg-teal-700 hover:bg-teal-800'
         onClick={goToDemo}>Go to Demo page
       </button>
       <button 
-        className='p-6 mb-4 rounded-md bg-teal-700'
+        className='p-6 mb-4 rounded-md bg-purple-700 hover:bg-purple-800'
         onClick={goToRegistration}>Go to Influencer registration page
       </button>
     </main>
